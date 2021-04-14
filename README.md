edc22c7bc187ea712b546bb714f51e76787cdd40
iloveslowfood <silkstaff@naver.com>
2021-04-14 13:21:42 +0000

    rename

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mini Chatbot
 simple chat bot using pretrained BERT from huggingface
+
+
+## Offline Task
--- chatbot.py
+++ chatbot.py
@@ -43,9 +43,3 @@ if __name__ == '__main__':
         if text == EXIT:
             break
         bot.query(text)
-
-    
-
-
-
-
